d92906f515164fca8f01cde90460727e326929e6
Gilson Santos <gilsonjosert@gmail.com>
2021-09-09 03:48:25 +0000

    create component Navigation + page Listagem-block

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,69 @@
-export default class navigation extends HTMLElement{
+class Navigation extends HTMLElement{
     constructor(){
-        this.super();
+        super();
+
+        const shadow = this.attachShadow({mode:'open'});
+        // const template = document.getElementById('navigation');
+        // const templateContent = template.content;
+        const tp = document.createElement('template');
+        tp.setAttribute('id','navigation');
+        tp.innerHTML = `
+            <nav>
+                <div class="nav-container">
+                    <slot name="brand"></slot>
+                    <slot name="search"></slot>
+                    <slot name="actions"></slot>
+                </div>
+            </nav>
+        `;
+
+        const bg = this.getAttribute('bg');
+        this.style = `width:100%;`;
+
+        const style = document.createElement('style');
+        style.textContent = `
+            nav{
+                max-height:var(--scale-xextra);
+                padding:var(--scale-medium) var(--scale-xbig);
+                display:flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+            
+                background:${bg =='' || bg==undefined ? '#181818'  : 'var(--color-'+bg+'-3)'};
+            }
+            
+            nav .nav-container{
+                width:var(--width-main);
+                display:flex;
+                gap:var(--scale-big);
+                flex-direction: row;
+                justify-content: space-between;
+                align-items: center;
+            }
+
+            @media only screen and (max-width: 1240px)  {
+                nav .nav-container{
+                    width:var(--width-fullscreen);
+                }
+            }
+            
+            @media only screen and (max-width:640px){
+                nav .nav-container{
+                    place-content: center center;
+                }
+                nav .nav-container a{
+                    margin:0;
+                }
+                nav .nav-container button,nav .nav-container input{
+                    display:none;
+                }
+            }
+        `;
+        shadow.appendChild(style);
+        shadow.appendChild(tp.content.cloneNode(true));
     }
 }
+
+
+customElements.define('navigation-top',Navigation);
---
+++ src/pages/ListagemCategoria/script.js
@@ -0,0 +1,2 @@
+// const js = document.createElement("script").setAttribute('src',"../../scripts/index.js");
+// document.querySelector("head").appendChild(js);
