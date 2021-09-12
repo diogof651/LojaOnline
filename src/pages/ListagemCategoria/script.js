430782c965bb4352b6241cc5f084178ac45c1f51
Gilson Santos <gilsonjosert@gmail.com>
2021-09-12 20:25:39 +0000

    Implement list/block view list

--- src/pages/ListagemCategoria/script.js
+++ src/pages/ListagemCategoria/script.js
@@ -1,2 +1,10 @@
 // const js = document.createElement("script").setAttribute('src',"../../scripts/index.js");
 // document.querySelector("head").appendChild(js);
+
+const buttonList = document.querySelector('section>.lead-actions>button:nth-child(1)');
+
+buttonList.addEventListener('click',(_)=>{
+    const list = document.querySelector('section.list-products');
+    list.classList.toggle('list');
+    buttonList.classList.toggle('grid');
+});
